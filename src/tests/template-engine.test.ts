d42803bed258233c6f42ad988c3ab753832275dd
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-04-29 00:32:30 +0200

    feat: should replace the single variable with the provided value

--- src/tests/template-engine.test.ts
+++ src/tests/template-engine.test.ts
@@ -1,12 +1,27 @@
 import { test, expect } from "@jest/globals";
 
-function returnTemplate(templateString: string): string {
-  return templateString;
+function returnTemplate(
+  templateString: string,
+  variables: Record<string, string>,
+): string {
+  const key = Object.keys(variables).at(0);
+  const value = Object.values(variables).at(0);
+  if (!key || !value) {
+    return templateString;
+  }
+  return templateString.replace(`{${key}}`, value);
 }
 
 test("should return the same template if no variables are provided", () => {
-  const result = returnTemplate("Hello, {name}!");
+  const result = returnTemplate("Hello, {name}!", {});
   const expected = "Hello, {name}!";
+
+  expect(result).toBe(expected);
+});
+
+test("should replace the single variable with the provided value", () => {
+  const result = returnTemplate("Hello, {name}!", { name: "John" });
+  const expected = "Hello, John!";
 
   expect(result).toBe(expected);
 });
