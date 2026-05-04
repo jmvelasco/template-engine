9a90941f8b1d6172a808ab62be5f69fc2fe4d458
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-05-05 00:54:17 +0200

    test: add describe and update test cases description

--- src/tests/template-engine.test.ts
+++ src/tests/template-engine.test.ts
@@ -1,60 +1,65 @@
-import { test, expect } from "@jest/globals";
+import { test, expect, describe } from "@jest/globals";
 import { render } from "../core/template-engine";
 
-test("should return the template unchanged if there are no placeholders", () => {
-  const result = render("Hello, world!", {});
-  const expected = "Hello, world!";
+describe("The render function", () => {
+  test("should return the template unchanged if there are no placeholders", () => {
+    const result = render("Hello, world!", {});
+    const expected = "Hello, world!";
 
-  expect(result).toBe(expected);
-});
-
-test("should return the same template if no variables are provided", () => {
-  const result = render("Hello, ${name}!", {});
-  const expected = "Hello, ${name}!";
-
-  expect(result).toBe(expected);
-});
-
-test("should return the template as is when variables are provided but no placeholders match", () => {
-  const result = render("Hello, ${name}!", { age: "21" });
-  const expected = "Hello, ${name}!";
-
-  expect(result).toBe(expected);
-});
-
-test("should replace the placeholder following the pattern ${variable_name}", () => {
-  const result = render("Hello, ${name}!", { name: "John" });
-  const expected = "Hello, John!";
-
-  expect(result).toBe(expected);
-});
-
-test("should replace all occurrences of the placeholder following the pattern ${variable_name}", () => {
-  const result = render("Hello, ${name}! Welcome, ${name}!", { name: "John" });
-  const expected = "Hello, John! Welcome, John!";
-
-  expect(result).toBe(expected);
-});
-
-test("should replace all occurrences of the placeholder following the pattern ${variable_name}", () => {
-  const result = render("Here it is ${name}!, he is ${age} years old.", {
-    name: "John",
-    age: "21",
+    expect(result).toBe(expected);
   });
-  const expected = "Here it is John!, he is 21 years old.";
 
-  expect(result).toBe(expected);
-});
+  test("should return the same template if no variables are provided", () => {
+    const result = render("Hello, ${name}!", {});
+    const expected = "Hello, ${name}!";
 
-test("should replace all occurrences of the placeholder following the pattern ${variable_name}", () => {
-  const result = render(
-    "Here it is ${name}!, he is ${age} years old and his sister is ${sisterAge}.",
-    {
+    expect(result).toBe(expected);
+  });
+
+  test("should return the template as is when variables are provided but no placeholders match", () => {
+    const result = render("Hello, ${name}!", { age: "21" });
+    const expected = "Hello, ${name}!";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should replace the placeholder following the pattern ${variable_name}", () => {
+    const result = render("Hello, ${name}!", { name: "John" });
+    const expected = "Hello, John!";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should replace all occurrences of the placeholder following the pattern ${variable_name}", () => {
+    const result = render("Hello, ${name}! Welcome, ${name}!", {
+      name: "John",
+    });
+    const expected = "Hello, John! Welcome, John!";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should replace occurrences with multiple placeholders following the pattern ${variable_name}", () => {
+    const result = render("Here it is ${name}!, he is ${age} years old.", {
       name: "John",
       age: "21",
-      sisterAge: "25",
-    },
-  );
-  const expected = "Here it is John!, he is 21 years old and his sister is 25.";
-  expect(result).toBe(expected);
+    });
+    const expected = "Here it is John!, he is 21 years old.";
+
+    expect(result).toBe(expected);
+  });
+
+  test("should replace all occurrences with multiple placeholders following the pattern ${variable_name}", () => {
+    const result = render(
+      "Here it is ${name}!, he is ${age} years old and the sister of ${name} is ${sisterAge}.",
+      {
+        name: "John",
+        age: "21",
+        sisterAge: "25",
+      },
+    );
+    const expected =
+      "Here it is John!, he is 21 years old and the sister of John is 25.";
+    expect(result).toBe(expected);
+  });
 });
