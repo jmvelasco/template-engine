1546921904ce0d24845b4773d46734cfe72ba6d9
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-05-12 04:29:42 +0200

    test(refactor): removed legacy root src directory

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.test.ts',
-    '!src/**/*.spec.ts',
+  projects: [
+    '<rootDir>/apps/backend',
   ],
-  coverageDirectory: 'coverage',
+  coverageDirectory: '<rootDir>/coverage',
   coverageReporters: ['text', 'lcov', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   verbose: true,
 };
